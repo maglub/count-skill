c50056f6a9c54c96f4a08a5c1417c1efaff27900
Magnus Lübeck <magnus.lubeck@gmail.com>
2019-05-10 12:17:58 +0200

    bapp

--- getBeerTemperature.sh
+++ getBeerTemperature.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-curl --silent -G 'http://192.168.4.55:8086/query?pretty=true' --data-urlencode "db=smokerWue" --data-urlencode "q=SELECT last(\"value\") FROM \"sensor_data\" WHERE (\"sensor\" = 'smoker-temp')" | jq ".results[].series[].values[]| .[1]"
+t=`curl --silent -G 'http://192.168.4.55:8086/query?pretty=true' --data-urlencode "db=smokerWue" --data-urlencode "q=SELECT last(\"value\") FROM \"sensor_data\" WHERE (\"sensor\" = 'smoker-temp')" | jq ".results[].series[].values[]| .[1]"`
 
+echo "t: $t C" >> /tmp/magnus.out
 date >> /tmp/magnus.out
