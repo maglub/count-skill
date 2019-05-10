0c58620597ae3d8057eb681985ed1b522754df34
Magnus Lübeck <magnus.lubeck@gmail.com>
2019-05-10 12:24:32 +0200

    bapp

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ class Count(MycroftSkill):
 #            number = int(message.data.get("number"))
 #            response = {'number': message.data.get("number")}
 #            self.speak_dialog("count_start", data=response)
-            self.speak("the beer is " + temperature.rstrip() + " degrees celcius")
+            self.speak("the beer is " + str(temperature) + " degrees celcius")
 #            for i in range(1, number+1, +1):
 #                self.speak("magnus " + str(i) + " .")
 #            self.speak_dialog("count_stop")
